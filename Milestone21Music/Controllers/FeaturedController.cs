53839d8b89d2e2dafde8de9b6bef27b8c8098c03
Steve The Main Man <Steve The Main Man@DESKTOP-IMCRT63>
2017-03-13 19:03:46 +0200

    Add project files.

---
+++ Milestone21Music/Controllers/FeaturedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Milestone21Music.Controllers
+{
+    public class FeaturedController : Controller
+    {
+        // GET: Featured
+        public ActionResult Featured()
+        {
+            return View();
+            //hahadhaha
+            //Awe what kind 
+
+        }
+    }
+}
---
+++ Milestone21Music/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Milestone21Music.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Rhythmn.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact details can be found below:";
+
+            return View();
+        }
+
+        public ActionResult BedroomProudcerDiaries()
+        {
+            return View();
+        }
+
+        public ActionResult Music()
+        {
+            //this method takes the user to the music page with iframes of various soundclouds links.
+
+            return View();
+        }
+    }
+}
